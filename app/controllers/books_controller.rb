7257f3520668dcc3753c3c6f25bdf73993eda6b0
t-Nakamura <1am1goodtime@gmail.com>
2021-07-28 14:56:09 +0000

    validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book succcessfully."
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book succcessfully."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
