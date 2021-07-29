43ea18e43154c0f656585de5b74fa832e6f598f2
t-Nakamura <1am1goodtime@gmail.com>
2021-07-29 14:04:12 +0000

    book/edit_validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,9 +32,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "You have updated book succcessfully."
-    redirect_to book_path(@book)
+      if @book.update(book_params)
+        flash[:notice] = "You have updated book succcessfully."
+        redirect_to book_path(@book)
+      else
+        render :edit
+      end
   end
 
   def destroy
